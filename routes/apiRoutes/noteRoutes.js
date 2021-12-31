8d859ae38e79d7d480e967e7309596257635f0c7
unknown <chesebrojared@gmail.com>
2021-12-31 07:15:43 -0800

    note routes outlined

---
+++ routes/apiRoutes/noteRoutes.js
@@ -0,0 +1,17 @@
+const { Router } = require('express');
+
+//Dependencies
+const fs = require("fs");
+const path = require('path');
+
+Router.get('/notes', (req, res) => {
+
+})
+
+Router.post('/notes', (req, res) => {
+
+})
+
+Router.delete('/notes/:id', (req, res) => {
+    
+})
