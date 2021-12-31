fedf666127c86d6dac0f8467c0ea0163b03a057e
unknown <chesebrojared@gmail.com>
2021-12-31 07:37:23 -0800

    import functions to noteRoutes

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -4,6 +4,9 @@ const { Router } = require('express');
 const fs = require("fs");
 const path = require('path');
 
+//import functions
+const { newNote, validateNote, getId } = require('../../lib/notes');
+
 Router.get('/notes', (req, res) => {
 
 })
@@ -13,5 +16,5 @@ Router.post('/notes', (req, res) => {
 })
 
 Router.delete('/notes/:id', (req, res) => {
-    
+
 })
